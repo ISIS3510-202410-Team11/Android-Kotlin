9453b3e9a95861841aefdc90d6c01d3db29cf674
Paola Campiño <78177442+Paolaaaaaa@users.noreply.github.com>
2024-02-28 02:03:14 -0500

    inic

---
+++ app/src/main/java/com/example/shareride/activities/logIn/LogInActivity.kt
@@ -0,0 +1,12 @@
+package com.example.shareride.activities.logIn
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shareride.R
+
+class logInActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_log_in)
+    }
+}
