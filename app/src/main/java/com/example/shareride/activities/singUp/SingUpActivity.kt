2841dd1d2ceca1dce05b45b1d630cc35ba9886f4
Paola Campiño <78177442+Paolaaaaaa@users.noreply.github.com>
2024-03-03 20:48:21 -0500

    avance

--- app/src/main/java/com/example/shareride/activities/singUp/SingUpActivity.kt
+++ app/src/main/java/com/example/shareride/activities/singUp/SingUpActivity.kt
@@ -1,12 +1,20 @@
-package com.example.shareride.activities
+package com.example.shareride.activities.singUp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toolbar
+import androidx.activity.compose.setContent
 import com.example.shareride.R
 
 class SingUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sing_up)
+        //setContentView(R.layout.activity_sing_up)
+        setContent {
+            Toolbar toolbar = (Toolbar) findv
+        }
+
     }
 }
