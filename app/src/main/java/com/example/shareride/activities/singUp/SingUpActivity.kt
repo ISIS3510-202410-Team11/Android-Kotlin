fee21d6f9f7008e2fa3b18e2da05ed6ec03dc07e
Paola Campiño <78177442+Paolaaaaaa@users.noreply.github.com>
2024-03-05 02:47:19 -0500

    sing up complete

--- app/src/main/java/com/example/shareride/activities/singUp/SingUpActivity.kt
+++ app/src/main/java/com/example/shareride/activities/singUp/SingUpActivity.kt
@@ -1,20 +1,32 @@
 package com.example.shareride.activities.singUp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageButton
 import android.widget.Toolbar
 import androidx.activity.compose.setContent
 import com.example.shareride.R
+import com.example.shareride.activities.StartActivity.StartActivity
+import com.example.shareride.activities.logIn.logInActivity
 
 class SingUpActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
 
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_sing_up)
-        setContent {
-            Toolbar toolbar = (Toolbar) findv
+        setContentView(R.layout.activity_sing_up)
+        val closeButton: ImageButton = findViewById(R.id.cancel_button)
+
+        closeButton.setOnClickListener {
+
+            val intent = Intent(this,StartActivity::class.java)
+            startActivity(intent)
         }
 
-    }
+
+}
 }
--- app/src/main/java/com/example/shareride/activities/startActivity/StartActivity.kt
+++ app/src/main/java/com/example/shareride/activities/startActivity/StartActivity.kt
@@ -1,12 +1,30 @@
 package com.example.shareride.activities.StartActivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import androidx.compose.ui.tooling.preview.Preview
 import com.example.shareride.R
-
+import com.example.shareride.activities.singUp.SingUpActivity
 class StartActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
+        val singUpButton: Button = findViewById(R.id.singUpbutton)
+        val loginInButton: Button = findViewById(R.id.logInbutton)
+
+        singUpButton.setOnClickListener{
+            val intent = Intent(this, SingUpActivity::class.java)
+            startActivity(intent)
+
+
+        }
+
+
+
+
     }
 }
