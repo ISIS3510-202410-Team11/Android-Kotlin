a8235812a8afff31fa7ce0b049fad1173e5b7779
Paola Campiño <78177442+Paolaaaaaa@users.noreply.github.com>
2024-03-10 15:04:56 -0500

    rename

--- app/src/main/java/com/example/shareride/HomeDriverFragment.kt
+++ app/src/main/java/com/example/shareride/HomeDriverFragment.kt
@@ -36,7 +36,7 @@ class HomeDriverFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home_driver, container, false)
+        return inflater.inflate(R.layout.fragment_home_passenger, container, false)
     }
 
 
