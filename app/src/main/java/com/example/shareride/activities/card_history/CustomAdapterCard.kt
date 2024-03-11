cd362c291ba183d5a44e0ad2a9d046aad9f769b6
Paola Campiño <78177442+Paolaaaaaa@users.noreply.github.com>
2024-03-11 18:39:16 -0500

    driver vehicles

--- app/src/main/java/com/example/shareride/activities/card_history/CustomAdapterCard.kt
+++ app/src/main/java/com/example/shareride/activities/card_history/CustomAdapterCard.kt
@@ -6,6 +6,7 @@ import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.shareride.R
+import com.example.shareride.clases.Transport
 
 class CustomAdapterCard(private val titles: List<String>, private val directions: List<String>) :
     RecyclerView.Adapter<CustomAdapterCard.ViewHolder>() {
---
+++ app/src/main/java/com/example/shareride/clases/Transport.kt
@@ -0,0 +1,6 @@
+package com.example.shareride.clases
+
+ class Transport(val name: String, val type: String, val plate: String, val reference:String) {
+
+
+ }
