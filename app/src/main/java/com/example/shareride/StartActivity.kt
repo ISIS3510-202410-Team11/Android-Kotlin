9f6fa031e5df98b857bf2df7eae4ecb6f531229f
thalassamania <j.charryg@uniandes.edu.co>
2024-03-17 20:35:17 -0700

    Display_Advancements

--- app/src/main/java/com/example/shareride/StartActivity.kt
+++ app/src/main/java/com/example/shareride/StartActivity.kt
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import com.example.shareride.activities.singUp.SingUpActivity
+import com.example.shareride.activities.logIn.logInActivity
 class StartActivity : AppCompatActivity() {
 
 
@@ -12,17 +13,17 @@ class StartActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
         val singUpButton: Button = findViewById(R.id.singUpbutton)
-        //val loginInButton: Button = findViewById(R.id.logInbutton)
+        val loginInButton: Button = findViewById(R.id.logInbutton)
 
         singUpButton.setOnClickListener{
             val intent = Intent(this, SingUpActivity::class.java)
             startActivity(intent)
-
-
         }
 
-
-
+        loginInButton.setOnClickListener{
+            val intent = Intent(this, logInActivity::class.java)
+            startActivity(intent)
+        }
 
     }
 }
--- app/src/main/java/com/example/shareride/activities/logIn/LogInActivity.kt
+++ app/src/main/java/com/example/shareride/activities/logIn/LogInActivity.kt
@@ -1,12 +1,22 @@
 package com.example.shareride.activities.logIn
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 import com.example.shareride.R
+import com.example.shareride.StartActivity
 
 class logInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_log_in)
+        val closeButton: ImageButton = findViewById(R.id.cancel_button)
+        closeButton.setOnClickListener {
+
+            val intent = Intent(this, StartActivity::class.java)
+            startActivity(intent)
+        }
     }
+
 }
