8ab5de981fcb7bb6f83f443d81c9cafdef1d42a8
thalassamania <j.charryg@uniandes.edu.co>
2024-03-22 00:08:26 -0700

    Avance Forgot Password

--- app/src/main/java/com/example/shareride/StartActivity.kt
+++ app/src/main/java/com/example/shareride/StartActivity.kt
@@ -4,9 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
-import com.example.shareride.activities.mainActivity.MainActivityPassenger
 import com.example.shareride.activities.singUp.SingUpActivity
-import com.example.shareride.activities.logIn.logInActivity
+import com.example.shareride.activities.logIn.LogInActivity
 class StartActivity : AppCompatActivity() {
 
 
@@ -14,30 +13,21 @@ class StartActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_start)
         val singUpButton: Button = findViewById(R.id.singUpbutton)
-<<<<<<< HEAD
-        val loginInButton: Button = findViewById(R.id.logInbutton)
-=======
         val loginInButton: Button = findViewById(R.id.logInButton)
->>>>>>> 821c9b64a48aff25c0b0a1be937601e37f5f1ead
 
         singUpButton.setOnClickListener{
             val intent = Intent(this, SingUpActivity::class.java)
             startActivity(intent)
+
+
         }
 
-<<<<<<< HEAD
-        loginInButton.setOnClickListener{
-            val intent = Intent(this, logInActivity::class.java)
-            startActivity(intent)
-        }
-=======
         loginInButton.setOnClickListener {
-            val intent_t = Intent(this, MainActivityPassenger::class.java)
-            startActivity(intent_t)
+            val intentLogIN = Intent(this, LogInActivity::class.java)
+            startActivity(intentLogIN)
         }
 
 
->>>>>>> 821c9b64a48aff25c0b0a1be937601e37f5f1ead
 
     }
 }
